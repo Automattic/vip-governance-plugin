7282e20d82fe9b5fe07e72cbe4d5644aec8feadd
Alec Geatches <alec.geatches@automattic.com>
2023-05-31 13:37:33 -0600

    Refactor block locking code for hardcoded rules

--- src/interactions/block-locking.php
+++ src/interactions/block-locking.php
@@ -5,35 +5,31 @@ namespace WPCOMVIP\Governance\Interactions;
 defined( 'ABSPATH' ) || die();
 
 class BlockLocking {
+	private static $is_lockdown_mode = false;
+	private static $allowed_blocks   = [
+		'core/image',
+		'core/gallery',
+	];
+
 	public static function init() {
-		add_filter(
-			'block_editor_settings_all',
-			function( $settings, $context ) {
-				// See https://gutenberg.10up.com/reference/Blocks/block-locking/#override-block-locking-ui-setting
-
-				// Disable unlocking blocks
-				$settings['canLockBlocks'] = false;
-
-				// Disable switching to code editing mode
-				$settings['codeEditingEnabled'] = false;
-				return $settings;
-			},
-			10,
-			2
-		);
-		add_filter('allowed_block_types_all', function() {
-			return [
-				'core/image',
-				'core/gallery',
-			];
-		});
+		if ( self::$is_lockdown_mode ) {
+			add_filter( 'block_editor_settings_all', [ __CLASS__, 'lockdown_block_editor_settings' ], 10, 2 );
+			add_filter( 'allowed_block_types_all', [ __CLASS__, 'lockdown_allowed_block_types' ] );
+		}
 	}
 
-	private static function get_rules() {
-		$rules_json = file_get_contents( __DIR__ . '/block-locking.json' );
-		$rules      = json_decode( $rules_json, /* associative */ true );
+	public static function lockdown_block_editor_settings( $settings, $context ) {
+		// Disable unlocking blocks
+		$settings['canLockBlocks'] = false;
 
-		return $rules['blockLockingRules'] ?? [];
+		// Disable switching to code editing mode
+		$settings['codeEditingEnabled'] = false;
+
+		return $settings;
+	}
+
+	public static function lockdown_allowed_block_types( $settings, $context ) {
+		return self::$allowed_blocks;
 	}
 }
 
