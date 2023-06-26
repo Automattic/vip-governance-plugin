0bc603af5cbd205ed1c39633c9a91329346a571c
Alec Geatches <alec.geatches@automattic.com>
2023-06-26 14:35:47 -0600

    Change lockout code to avoid double-locking inner blocks to prevent CSS layout issues

--- src/block-locking.jsx
+++ src/block-locking.jsx
@@ -4,10 +4,8 @@
 import { Disabled } from '@wordpress/components';
 import { addFilter } from '@wordpress/hooks';
 import { createHigherOrderComponent } from '@wordpress/compose';
-import {
-	store as blockEditorStore,
-	privateApis as blockEditorPrivateApis,
-} from '@wordpress/block-editor';
+import { store as blockEditorStore, privateApis } from '@wordpress/block-editor';
+import { select } from '@wordpress/data';
 
 /**
  * Private WordPress dependencies
@@ -20,7 +18,7 @@ export const { lock, unlock } =
 		// (not the name of the package whose APIs you want to access)
 	);
 
-const { useBlockEditingMode } = unlock( blockEditorPrivateApis );
+const { useBlockEditingMode } = unlock( privateApis );
 
 /**
  * Internal dependencies
@@ -30,7 +28,21 @@ import { doesBlockNameMatchBlockRegex } from './block-utils';
 export function setupBlockLocking( allowedBlocks ) {
 	const withDisabledBlocks = createHigherOrderComponent( BlockEdit => {
 		return props => {
-			const blockName = props.name;
+			const { name: blockName, clientId } = props;
+
+			const { getBlockParents, getBlockAttributes } = select( blockEditorStore );
+			const parentClientIds = getBlockParents(clientId, true);
+			const isParentLocked = parentClientIds.some( parentClientId => {
+				const parentAttributes = getBlockAttributes( parentClientId );
+
+				return parentAttributes['vip-locked'] === true;
+			});
+
+			if ( isParentLocked ) {
+				// To avoid layout issues, only disable the outermost locked block
+				return <BlockEdit { ...props } />;
+			}
+
 			const isAllowed = allowedBlocks.some( allowedBlock => doesBlockNameMatchBlockRegex( blockName, allowedBlock ) );
 
 			if ( isAllowed ) {
@@ -40,6 +52,9 @@ export function setupBlockLocking( allowedBlocks ) {
 				// Valid block editing modes are 'disabled', 'contentOnly', or 'default':
 				// https://github.com/WordPress/gutenberg/blob/075d937/packages/block-editor/src/components/block-editing-mode/index.js#L30-L36
 				useBlockEditingMode( 'disabled' );
+
+				// Set 'vip-locked' so that children can detect they're within an existing locked block
+				props.setAttributes({ 'vip-locked': true });
 
 				return (
 					<Disabled>
