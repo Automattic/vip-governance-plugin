7a7e8c9351c5293451bf8de61497197a6d470dcb
Alec Geatches <alec.geatches@automattic.com>
2023-06-26 11:53:01 -0600

    Add block locking with useBlockEditingMode

--- src/block-locking.jsx
+++ src/block-locking.jsx
@@ -1,6 +1,20 @@
-import { Disabled } from '@wordpress/components';
+import { Disabled,  } from '@wordpress/components';
 import { addFilter } from '@wordpress/hooks';
 import { createHigherOrderComponent } from '@wordpress/compose';
+import {
+	store as blockEditorStore,
+	privateApis as blockEditorPrivateApis,
+} from '@wordpress/block-editor';
+
+import { __dangerousOptInToUnstableAPIsOnlyForCoreModules } from '@wordpress/private-apis';
+export const { lock, unlock } =
+	__dangerousOptInToUnstableAPIsOnlyForCoreModules(
+		'I know using unstable features means my plugin or theme will inevitably break on the next WordPress release.',
+		'@wordpress/block-editor' // Name of the package calling __dangerousOptInToUnstableAPIsOnlyForCoreModules,
+		// (not the name of the package whose APIs you want to access)
+	);
+
+const { useBlockEditingMode } = unlock( blockEditorPrivateApis );
 
 export function setupBlockLocking( allowedBlocks ) {
 	const withDisabledBlocks = createHigherOrderComponent( BlockEdit => {
@@ -9,14 +23,20 @@ export function setupBlockLocking( allowedBlocks ) {
 
 			if ( isAllowed ) {
 				return <BlockEdit { ...props } />;
+			} else {
+				// Warning: Unstable API.
+				// Valid block editing modes are 'disabled', 'contentOnly', or 'default':
+				// https://github.com/WordPress/gutenberg/blob/075d937/packages/block-editor/src/components/block-editing-mode/index.js#L30-L36
+				useBlockEditingMode( 'disabled' );
+
+				return (
+					<Disabled>
+						<div style={ { opacity: 0.6, 'background-color': '#eee', border: '2px dashed #999' } }>
+							<BlockEdit { ...props } />
+						</div>
+					</Disabled>
+				);
 			}
-			return (
-				<Disabled>
-					<div style={ { opacity: 0.6, 'background-color': '#eee', border: '2px dashed #999' } }>
-						<BlockEdit { ...props } />
-					</div>
-				</Disabled>
-			);
 		};
 	}, 'withDisabledBlocks' );
 
