df109f85498a55e203dfae2c11187bcbdff15e33
ingeniumed <ingeniumed@users.noreply.github.com>
2023-07-04 15:00:00 +1000

    Show the validation result for the schema in the admin panel, as well as debug info related to the plugin

--- governance/wp-admin-panel.php
+++ governance/wp-admin-panel.php
@@ -2,9 +2,20 @@
 
 namespace WPCOMVIP\Governance;
 
+defined( 'ABSPATH' ) || die();
+
+use Exception;
+
 class Admin {
+	private static $governance_errors = null;
 	
 	public static function init() {
+		try {
+			InitGovernance::get_governance_rules( WPCOMVIP_GOVERNANCE_RULES_FILENAME, true );
+		} catch ( Exception $e ) {
+			self::$governance_errors = $e->getMessage();
+		}
+
 		add_action( 'admin_menu', [ __CLASS__, 'governance_plugin_setup_menu' ] );
 	}
 
@@ -17,8 +28,21 @@ class Admin {
 		// 1: Debug Info related to the plugin
 		// 2: Schema Validation results
 		// 3: Ability to turn off the plugin without re-deploying
-		echo '<h1> VIP Governance </h1>';
+		echo '<div class="wrap">';
+		echo '<h1>VIP Governance</h1>';
+		echo '<h2> Debug Information </h2>';
+		echo '<p>Plugin Version: ' . esc_html( WPCOMVIP__GOVERNANCE__PLUGIN_VERSION ) . '</p>';
+		echo '<h2>Schema Validation Result:</h2>';
+		// only print the governance_error if its not null, otherwise output no errors found
+		if ( null === self::$governance_errors ) {
+			echo '<p>No errors found</p>';
+		} else {
+			// escape and print the output of the governance_errors
+			echo '<pre>' . esc_html( self::$governance_errors ) . '</pre>';
+		}
+		echo '</div>';
 	}
+
 }
 
 Admin::init();
