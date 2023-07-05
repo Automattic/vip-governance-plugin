41d859c657e8711fb5be7dc5b89d07e62a1cde18
Alec Geatches <alec.geatches@automattic.com>
2023-07-05 12:14:40 -0600

    Refactor block-locking to use local variables and filter closure

--- governance/block-locking.php
+++ governance/block-locking.php
@@ -5,33 +5,30 @@ namespace WPCOMVIP\Governance;
 defined( 'ABSPATH' ) || die();
 
 class BlockLocking {
-	private static $should_block_locking_be_disabled = true;
-	private static $should_code_editor_be_disabled   = true;
-
 	public static function init( $allowed_features ) {
-		if ( ! in_array( 'moveBlocks', $allowed_features ) ) {
-			self::$should_block_locking_be_disabled = false;
+		$allow_move_blocks = false;
+		$allow_code_editor = false;
+
+		if ( in_array( 'moveBlocks', $allowed_features ) ) {
+			$allow_move_blocks = true;
 		}
 
-		if ( ! in_array( 'codeEditor', $allowed_features ) ) {
-			self::$should_code_editor_be_disabled = false;
+		if ( in_array( 'codeEditor', $allowed_features ) ) {
+			$allow_code_editor = true;
 		}
 
-		add_filter( 'block_editor_settings_all', [ __CLASS__, 'lockdown_block_editor_settings' ], 10, 2 );
+		add_filter( 'block_editor_settings_all', function( $settings, $context ) use ( $allow_move_blocks, $allow_code_editor ) {
+			if ( false === $allow_move_blocks ) {
+				// Disable unlocking blocks
+				$settings['canLockBlocks'] = false;
+			}
+
+			if ( false === $allow_code_editor ) {
+				// Disable switching to code editing mode
+				$settings['codeEditingEnabled'] = false;
+			}
+
+			return $settings;
+		}, 10, 2 );
 	}
-
-	public static function lockdown_block_editor_settings( $settings, $context ) {
-		if ( self::$should_block_locking_be_disabled ) {
-			// Disable unlocking blocks
-			$settings['canLockBlocks'] = false;
-		}
-
-		if ( self::$should_code_editor_be_disabled ) {
-			// Disable switching to code editing mode
-			$settings['codeEditingEnabled'] = false;
-		}
-
-		return $settings;
-	}
-
 }
