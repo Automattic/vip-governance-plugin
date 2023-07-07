1b3adb0215714032b236ae0dfe49f471cac63cf9
Alec Geatches <alec.geatches@automattic.com>
2023-07-07 14:52:53 -0600

    Add built JS to repository files

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '3e6ef7dcd36cc54cab1b');
