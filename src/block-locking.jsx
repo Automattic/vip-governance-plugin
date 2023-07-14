e75646b49f09f00c181d59d0989925ba2fb42b87
Alec Geatches <alec.geatches@automattic.com>
2023-07-14 12:28:43 -0600

    Add more test code

--- src/block-locking.jsx
+++ src/block-locking.jsx
@@ -62,17 +62,23 @@ export function setupBlockLocking( governanceRules ) {
 				return blockAttributes;
 			}
 
-			const { lock } = blockAttributes;
-			const savedLock = lock ? lock : false;
-
-			return {
+			const lockedBlockAttributes = {
 				...blockAttributes,
 				lock: {
 					move: true,
 					remove: true,
-				},
-				'vip-governance-attribute-saved-lock': savedLock,
-			};
+				}
+			}
+
+			const { lock: currentLock, 'vip-governance-saved-lock': savedLock } = blockAttributes;
+
+			if ( ! savedLock ) {
+				// First time running this filter. Save the block's existing lock.
+				console.log('Saving lock on:', blockType.name);
+				lockedBlockAttributes['vip-governance-saved-lock'] = currentLock;
+			}
+
+			return lockedBlockAttributes;
 		};
 
 		addFilter(
@@ -81,16 +87,67 @@ export function setupBlockLocking( governanceRules ) {
 			withDisabledMove,
 		);
 
+		// const withDisabledMove = createHigherOrderComponent( BlockEdit => {
+		// 	return props => {
+		// 		const { name: blockName, clientId, attributes, setAttributes } = props;
+
+		// 		const isAllowed = governanceRules.allowedBlocks.some( allowedBlock => doesBlockNameMatchBlockRegex( blockName, allowedBlock ) );
+
+		// 		if ( isAllowed ) {
+		// 			return <BlockEdit { ...props } />;
+		// 		} else {
+		// 			// debug: Always set lock on load
+		// 			if ( !( attributes.lock?.move === true && attributes.lock?.remove === true ) ) {
+		// 				console.log('Adding lock to:', blockName);
+		// 				setAttributes({
+		// 					lock: {
+		// 						move: true,
+		// 						remove: true,
+		// 					},
+		// 				});
+		// 			}
+
+		// 			// const { lock: currentLock, 'vip-governance-saved-lock': savedLock } = attributes;
+
+		// 			// if ( savedLock === undefined ) {
+		// 			// 	console.log('Locking and storing existing lock for:', blockName, 'with value:', currentLock ? currentLock : false);
+		// 			// 	// First time running this filter. Save the block's existing lock options.
+		// 			// 	setAttributes({
+		// 			// 		lock: {
+		// 			// 			move: true,
+		// 			// 			remove: true,
+		// 			// 		},
+		// 			// 		'vip-governance-saved-lock': currentLock ? currentLock : false,
+		// 			// 	});
+		// 			// } else {
+		// 			// 	console.log(blockName, 'already has a savedLock');
+		// 			// }
+
+		// 			return <BlockEdit { ...props } />;
+		// 		}
+		// 	};
+		// }, 'withDisabledMove' );
+
+		// addFilter( 'editor.BlockEdit', 'wpcomvip-governance/with-disabled-move', withDisabledMove );
+
 		const restoreSavedLockAttribute = ( element, blockType, attributes ) => {
 			// skip if element is undefined
 			if ( ! element ) {
 				return;
 			}
 
-			// if ( attributes['vip-governance-attribute-saved-lock'] !== undefined ) {
-			// 	console.log('element:', element);
-			// 	console.log('attributes:', attributes);
-			// 	console.log('--------')
+			// debug: Always remove lock on save (loops infinitely)
+			console.log('Removing lock from:', blockType.name);
+			// delete attributes['lock'];
+
+			// if ( attributes['vip-governance-saved-lock'] === false ) {
+			// 	// This block previously had no lock, so remove the lock attribute.
+			// 	console.log('Removing role lock for:', blockType.name);
+			// 	delete attributes['lock'];
+			// } else if ( attributes['vip-governance-saved-lock'] ) {
+			// 	// Restore the block's saved lock.
+			// 	console.log('Restoring role lock for:', blockType.name, ':', attributes['vip-governance-saved-lock']);
+			// 	attributes['lock'] = attributes['vip-governance-saved-lock'];
 			// }
 
 			return element;
@@ -102,38 +159,15 @@ export function setupBlockLocking( governanceRules ) {
 			restoreSavedLockAttribute
 		);
 
-		// const restoreSavedLockAttribute = createHigherOrderComponent( BlockEdit => {
-		// 	return props => {
-		// 		const { name: blockName, clientId } = props;
-
-		// 		// ToDo: This doesn't support nested blocks if they aren't specified in the allowedChildren. That needs to be resolved.
-		// 		const isAllowed = true;
-
-		// 		if ( isAllowed ) {
-		// 			return <BlockEdit { ...props } />;
-		// 		} else {
-		// 			// Set 'vip-governance-locked' so that children can detect they're within an existing locked block
-		// 			props.setAttributes({ 'vip-governance-locked': true });
-
-		// 			return <>
-		// 				<Disabled>
-		// 					<div style={ { opacity: 0.6, 'background-color': '#eee', border: '2px dashed #999' } }>
-		// 						<BlockEdit { ...props } />
-		// 					</div>
-		// 				</Disabled>
-		// 			</>;
-		// 		}
-		// 	};
-		// }, 'withDisabledBlocks' );
-
-		// addFilter( 'editor.BlockEdit', 'wpcomvip-governance/restore-saved-lock', restoreSavedLockAttribute );
-
 		// const restoreSavedLockAttribute = ( props, blockType, attributes ) => {
-		// 	if ( attributes['vip-governance-attribute-saved-lock'] !== undefined ) {
+		// 	if ( blockType.name === 'core/quote' ) {
+		// 		console.log('blockType:', blockType.name);
 		// 		console.log('props:', props);
 		// 		console.log('attributes:', attributes);
-		// 		console.log('--------')
+		// 		console.log('--------');
 		// 	}
+
+		// 	delete attributes['lock'];
 
 		// 	return props;
 		// };
