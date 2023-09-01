e7cecc9b66e1f073acfb1bbd8d4c6c24a5405957
ingeniumed <ingeniumed@users.noreply.github.com>
2023-09-01 10:01:05 +1000

    Fix the snake case usage

--- governance/governance-utilities.php
+++ governance/governance-utilities.php
@@ -96,24 +96,30 @@ class GovernanceUtilities {
 		foreach ( RulesParser::RULE_TYPES as $priority ) {
 			// look up the rule in $governance_rules where the field type matches priority.
 			$governance_rules_for_priority = array_filter( $governance_rules, function( $rule ) use ( $priority, $user_roles, $post_type, $type_to_rules_map ) {
+				// Its required to have the type, and its corresponding types set unless you are the default rule in which case you only need type set to default.
 				if ( isset( $rule['type'] ) && $priority === $rule['type'] && ( 'default' === $priority || isset( $rule[ $type_to_rules_map[ $priority ] ] ) ) ) {
 					if ( 'default' === $priority ) {
 						return true;
 					} elseif ( 'role' === $priority ) {
+						// Only give back true if the roles match the current user.
 						return array_intersect( $user_roles, $rule['roles'] );
 					} elseif ( 'postType' === $priority ) {
+						// Only give back true if the current post type matches the post types allowed.
 						return in_array( $post_type, $rule['postTypes'], true );
 					}
 				}
-				
+
+				// Rule should be ignored if it doesn't match the needed criteria for priorities.
 				return false;
 			} );
 
 			if ( ! empty( $governance_rules_for_priority ) ) {
+				// Re-order the rule so that the 0 index is what's first, otherwise the index is preserved.
 				$governance_rules_for_priority = array_values( $governance_rules_for_priority );
-				$allowed_blocks                = isset( $governance_rules_for_priority[0]['allowedBlocks'] ) ? ( 'default' === $priority ? [ ...$allowed_blocks, ...$governance_rules_for_priority[0]['allowedBlocks'] ] : $governance_rules_for_priority[0]['allowedBlocks'] ) : $allowed_blocks;
-				$block_settings                = isset( $governance_rules_for_priority[0]['blockSettings'] ) ? ( 'default' === $priority ? array_merge_recursive( $block_settings, $governance_rules_for_priority[0]['blockSettings'] ) : $governance_rules_for_priority[0]['blockSettings'] ) : $block_settings;
-				$allowed_features              = isset( $governance_rules_for_priority[0]['allowedFeatures'] ) ? ( 'default' === $priority ? [ ...$allowed_features, ...$governance_rules_for_priority[0]['allowedFeatures'] ] : $governance_rules_for_priority[0]['allowedFeatures'] ) : $allowed_features;
+
+				$allowed_blocks   = self::get_allowed_blocks_or_features_for_rule_type( 'allowedBlocks', $allowed_blocks, $governance_rules_for_priority[0], $priority );
+				$block_settings   = self::get_block_settings_for_rule_type( $block_settings, $governance_rules_for_priority[0], $priority );
+				$allowed_features = self::get_allowed_blocks_or_features_for_rule_type( 'allowedFeatures', $allowed_features, $governance_rules_for_priority[0], $priority );
 			}
 		}
 
@@ -123,6 +129,57 @@ class GovernanceUtilities {
 			'blockSettings'   => $block_settings,
 			'allowedFeatures' => $allowed_features,
 		);
+	}
+
+	/**
+	 * Get the new allowedBlocks or allowedFeatures based on the rule type
+	 * 
+	 * The default rule's allowedBlocks and allowedFeatures is combined with the other rule types.
+	 * For non-default rule types, only one allowedBlocks and allowedFeatures can be picked. It's not combined together.
+	 *
+	 * @param string $allowed_type allowedBlocks or allowedFeatures.
+	 * @param array  $allowed_blocks_or_features allowedBlocks or allowedFeatures that have been combined so far.
+	 * @param array  $governance_rule current rule being processed.
+	 * @param string $rule_type type of rule being processed.
+	 * @return array allowedBlocks or allowedFeatures that have been combined so far.
+	 */
+	private static function get_allowed_blocks_or_features_for_rule_type( $allowed_type, $allowed_blocks_or_features, $governance_rule, $rule_type ) {
+		if ( isset( $governance_rule[ $allowed_type ] ) ) {
+			// For the default rule the allowedBlocks and allowedFeatures are combined together.
+			// Otherwise, there can only be one.
+			if ( 'default' === $rule_type ) {
+				return [ ...$allowed_blocks_or_features, ...$governance_rule[ $allowed_type ] ];
+			} else {
+				$allowed_blocks_or_features = $governance_rule[ $allowed_type ];
+			}
+		}
+
+		return $allowed_blocks_or_features;
+	}
+
+	/**
+	 * Get the new blockSettings based on the rule type
+	 * 
+	 * The default rule's blockSettings is combined with the other rule types.
+	 * For non-default rule types, only one blockSettings can be picked. It's not combined together.
+	 *
+	 * @param array  $block_settings blockSettings that have been combined so far.
+	 * @param array  $governance_rule current rule being processed.
+	 * @param string $rule_type type of rule being processed.
+	 * @return array blockSettings that have been combined so far.
+	 */
+	private static function get_block_settings_for_rule_type( $block_settings, $governance_rule, $rule_type ) {
+		if ( isset( $governance_rule['blockSettings'] ) ) {
+			// For the default rule the blockSettings are combined together.
+			// Otherwise, there can only be one.
+			if ( 'default' === $rule_type ) {
+				return array_merge_recursive( $block_settings, $governance_rule['blockSettings'] );
+			} else {
+				$block_settings = $governance_rule['blockSettings'];
+			}
+		}
+
+		return $block_settings;
 	}
 
 }
