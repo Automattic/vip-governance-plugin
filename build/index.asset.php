9282318d3b7b26db37b7cd9ac21565c68a0b2274
ingeniumed <ingeniumed@users.noreply.github.com>
2024-01-19 13:55:40 +1100

    Upgrade to php 8 and move the devDependencies to actual dependencies

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '8a8d06edcd84464b7ff9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '9d2b5ba1e7daf4269f90');
