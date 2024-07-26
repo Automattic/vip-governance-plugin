20ba326687cf5d68d61e487bf2e2faa5fc000e2e
ingeniumed <ingeniumed@users.noreply.github.com>
2024-07-26 14:15:40 +1000

    Add a new filter for customizing the default core block list

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => 'd934724263e1e2804c08');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '1d9c4ca3ca7d6cb6f97a');
